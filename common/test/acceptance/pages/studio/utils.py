62f6ff4f115d5c509f85d1c93ceffa372fb17cc4
Jay Zoldak <zoldak@edx.org>
2014-07-17 11:47:23 -0400

    Remove action chains from click_css

--- common/test/acceptance/pages/studio/utils.py
+++ common/test/acceptance/pages/studio/utils.py
@@ -2,7 +2,6 @@
 Utility methods useful for Studio page tests.
 """
 from bok_choy.promise import Promise
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 def click_css(page, css, source_index=0, require_notification=True):
@@ -14,9 +13,7 @@ def click_css(page, css, source_index=0, require_notification=True):
     If require_notification is False (default value is True), the method will return immediately.
     Otherwise, it will wait for the "mini-notification" to appear and disappear.
     """
-    buttons = page.q(css=css).filter(lambda el: el.size['width'] > 0)
-    target = buttons[source_index]
-    ActionChains(page.browser).click(target).release().perform()
+    page.q(css=css).filter(lambda el: el.size['width'] > 0).nth(source_index).click()
     if require_notification:
         wait_for_notification(page)
 
